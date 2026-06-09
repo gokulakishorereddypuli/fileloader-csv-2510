bc1ef0a4e386c8c5768e60b1b6c109e08d312057
98608225-ora030 98608225-ora030 <98608225-ora030>
2026-06-09 16:46:47 +0000

    Oracle Visual Builder created default content for Application fileloader-csv-2510 with type VBCS from 98608225-ora030 98608225-ora030

---
+++ webApps/vbcookbook/flows/fileloader-csv/pages/fileloader-csv-start-page.js
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c)2020, 2026, Oracle and/or its affiliates.
+ * Licensed under The Universal Permissive License (UPL), Version 1.0
+ * as shown at https://oss.oracle.com/licenses/upl/
+ */
+define([], function () {
+  "use strict";
+
+  class PageModule {
+    constructor() {}
+    /**
+     *
+     * @param {File} files
+     * @return {String}
+     */
+    readAndProcess(files) {
+      return new Promise((resolve) => {
+        try {
+          const file = files[0];
+          let fileContent = [];
+          const fileReader = new FileReader();
+
+          fileReader.readAsText(file);
+
+          fileReader.onload = (e) => {
+            let result = e.target.result;
+            let columns = [];
+            let size;
+            if (result) {
+              result = result.split("\n").map((i) => i.split(","));
+              columns = result[0].map((col) => ({
+                field: col,
+                headerText:
+                  col
+                    .replace(/([A-Z]+)/g, " $1")
+                    .charAt(0)
+                    .toUpperCase() + col.replace(/([A-Z]+)/g, " $1").slice(1),
+              }));
+              size = columns.length;
+              let tableData = (result.slice(1) || [])
+                .filter((r) => r.length === size)
+                .map((r, idx) => {
+                  const part = {};
+                  for (let colIdx = 0; colIdx < columns.length; colIdx++) {
+                    part[columns[colIdx].field] = r[colIdx];
+                  }
+                  return part;
+                });
+              resolve({
+                success: true,
+                result: {
+                  name: file.name,
+                  size: file.size,
+                  type: file.type,
+                  columns: columns,
+                  tableData: tableData,
+                },
+                error: {
+                  detail: "",
+                },
+              });
+            } else {
+              resolve({
+                success: false,
+
+                error: {
+                  detail: "Empty File : " + result,
+                },
+              });
+            }
+          };
+        } catch (err) {
+          resolve({
+            success: false,
+            error: {
+              detail: "Error while reading file : " + err.detail,
+            },
+          });
+        }
+      });
+    }
+  }
+
+  return PageModule;
+});
